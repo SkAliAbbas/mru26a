02f9fd3bc369e0688395426d063ee0a799c468d8
SkAliAbbas <shaikaliabbas10@gmail.com>
2026-03-05 12:06:06 +0530

    updated

---
+++ part4/page32a.js
@@ -0,0 +1,72 @@
+let customers = [
+  {
+    id: 1,
+    name: "John",
+    balance: 5000,
+    transactions: [],
+  },
+  {
+    id: 2,
+    name: "Amy",
+    balance: 5000,
+    transactions: [],
+  },
+];
+
+function checkBalance(id) {
+  const customer = customers.find((customer) => customer.id === id);
+  console.log(customer);
+}
+
+function deposit(id, amount) {
+  customers = customers.map((customer) => {
+    if (customer.id === id) {
+      return {
+        ...customer,
+        balance: customer.balance + amount,
+        transactions: [
+          ...customer.transactions,
+          {
+            type: "credit",
+            amount: amount,
+            date: Date.now(),
+          },
+        ],
+      };
+    } else {
+      return customer;
+    }
+  });
+}
+
+function withdraw(id,amount){
+  customers = customers.map((customer) => {
+    if(customer.id === id){
+      return {
+        ...customer,
+        balance: customer.balance - amount,
+        transactions: [
+          ...customer.transactions,
+          {
+            type:"debit",
+            amount: amount,
+            date: Date.now(),
+          },
+        ],
+      };
+    } else {
+      return customer;
+    }
+  });
+}
+
+function showTransactions(id){
+  const customer = customers.find((customer) =>customer.id === id);
+  console.log(customer.transactions);
+}
+deposit(1, 1500);
+withdraw(1, 500); 
+checkBalance(1)
+deposit(1, 1000);
+checkBalance(1)
+showTransactions(1)
